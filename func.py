ec2da552c776ad6a57f0eba22572276a4111504d
NikolaiBirolini <nikolai.birolini@gmail.com>
2022-01-27 18:08:17 +0100

    ajout func_mirroir

--- func.py
+++ func.py
@@ -1,4 +1,16 @@
 import string
 
 def func_mirroir(mot,val):
-    return -1
+    if val <0:
+        print("Error - val negative")
+        return False
+    
+    if len(mot) < 1:
+        print("Error - string vide")
+        return False
+    
+    if val > len(mot):
+        print("Error - val trop grand")
+        return False
+
+    return mot[:(val+1)]+''.join(reversed(mot[:(val+1)]))
