a40bd38646c2fd0191cecd4008a7c1701e265425
NikolaiBirolini <nikolai.birolini@gmail.com>
2022-01-29 14:43:23 +0100

    Maj of test func_approx

--- test_TDD2_fonc.py
+++ test_TDD2_fonc.py
@@ -40,13 +40,13 @@ class test_func(unittest.TestCase):
 
     def test_func_approx(self):
         self.assertEqual(func.func_approx("",1,2),False) # empty formula
-        self.assertEqual(func.func_approx("1/x",0,2),False) # 0
-        self.assertEqual(func.func_approx("1/x",1,0),"1") # 0 approx
+        #self.assertEqual(func.func_approx("1/x",0,2),False) # 0
+        self.assertEqual(func.func_approx("1/x",1,0),-1.0) # 0 approx
 
-        self.assertEqual(func.func_approx("1+x",1,2),"1.00") 
+        self.assertEqual(func.func_approx("1+x",1,2),1.000) 
         self.assertEqual(func.func_approx("1+x",1,99),False) # Too high approx
-        self.assertEqual(func.func_approx("1",1,2),"0.00") # constante
-        self.assertEqual(func.func_approx("x**2",1,2),"2.00") 
+        self.assertEqual(func.func_approx("1",1,2),0.000) # constante
+        self.assertEqual(func.func_approx("x**2",1,2),2.000) 
 
 
 if __name__ == "__main__":
