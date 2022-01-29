b6d01461c74fe4e60e4e74d6db7cee2059d3b430
NikolaiBirolini <nikolai.birolini@gmail.com>
2022-01-29 12:20:28 +0100

    func_deriv complete

--- func.py
+++ func.py
@@ -16,6 +16,14 @@ def func_mirroir(mot,val):
     return mot[:(val+1)]+''.join(reversed(mot[:(val+1)]))
 
 
-def func_deriv(val):
+def func_deriv(List_val):
     h_pas = 1
-    return -1
+    List_output = []
+
+    if len(List_val) < 2:
+        print("Error - Liste trop courte")
+        return False
+    
+    for val in range(len(List_val)-1):
+        List_output.append((List_val[val+1]-List_val[val])/h_pas)
+    return List_output
